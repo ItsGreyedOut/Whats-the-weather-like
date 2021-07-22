8b26fb9e9335c4473fb9d1721abd76c60a41ca6f
Grey Hardy <greyhardy7@gmail.com>
2021-07-22 18:14:25 -0400

    adding ignore file

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = '64f87a5da565472d7b90eeb6758c8851'
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = 'AIzaSyDZDIL6JqLaS4eBbXQYW7FiOb6g1Qu4vBc'
